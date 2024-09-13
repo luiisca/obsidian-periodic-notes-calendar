78dbdadbd06c9b4772a641c710f8ea60ced8b8e1
Luis Cadillo <luiscaf3r@gmail.com>
2024-09-13 09:51:23 -0500

    remove vite

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig, preview } from 'vite';
-
-export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		port: 3000
-	}
-});
